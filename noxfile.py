561d88cbba65c6fe10e5ce791845b38ccc39bdef
Hoel Bagard <hoel.bagard@gmail.com>
2023-05-10 16:58:35 +0900

    Added noxfile, removed pytests GA in favor of a nox one.

---
+++ noxfile.py
@@ -0,0 +1,17 @@
+import nox
+
+
+@nox.session  # (python=["3.9", "3.10", "3.11"])
+def tests(session) -> None:
+    """Run the test suite."""
+    session.install("-r", "requirements/requirements-test.txt")
+    session.install(".")
+    session.run("pytest", "-v", "tests")
+
+#     # Here we queue up the test coverage session to run next
+#     session.notify("coverage")
+
+# @nox.session
+# def coverage(session):
+#     session.install("coverage")
+#     session.run("coverage")
